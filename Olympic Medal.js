af946f008f4b5e8f5cd50e277d5436cc7c71e336
Mostafa Magdy <116667363+MostafaManga10@users.noreply.github.com>
2022-11-17 05:00:59 +0300

    Create Olympic Medal.js

---
+++ Olympic Medal.js
@@ -0,0 +1,139 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+#define flush fflush(stdout);
+#define no cout << "NO";
+#define yes cout << "YES";
+#define line cout << '\n';
+#define here cout << "here!";
+#define read_in freopen ("Manga.txt","r",stdin);
+#define gen_out freopen ("output.txt","w",stdout);
+#define IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define X first
+#define Y second
+#define vec vector
+#define vi vec<int>
+#define vvi vec<vi>
+#define vll vec<ll>
+#define vc vec<char>
+#define vb vec<bool>
+#define MP make_pair
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define pb push_back
+#define sz(v) ((int)((v).size()))
+#define clr(v, d) memset(v,d,sizeof(v))
+#define all(v) ((v).begin()),((v).end())
+#define rall(v) ((v).rbegin()),((v).rend())
+#define rep(i, v) for(int i=0; i<sz(v); ++i)
+#define rep1(i, v) for(int i=1; i<=sz(v); ++i)
+#define lp(i, n) for(int i=0; i<(int)n; ++i)
+#define lp1(i, n) for(int i=1; i<=(int)n; ++i)
+#define lpi(i, j, n) for(int i=j; i<(int)n; ++i)
+#define lpd(i, j, n) for(int i=j; i>=(int)n; --i)
+
+typedef double dl;
+typedef long long ll;
+typedef unsigned int ui;
+typedef long long int lli;
+typedef unsigned long long ull;
+vec<vi > adjlist;
+
+//Practice on BFS Is a Shortest path Algorithm
+vi BFSShortestPath1(int s) {
+    queue<int> q;
+    vi length(adjlist.size(), INT_MAX);
+    length[s] = 1;
+    int dep = 0, cur = s, size;
+    while (!q.empty()) {
+        int size = q.size();
+        while (size--) {
+            cur = q.front();
+            q.pop();
+            for (auto i: adjlist[cur])
+                if (length[i] == INT_MAX) {
+                    length[i] = dep + 1;
+                    q.push(i);
+                }
+        }
+        dep++;
+    }
+    return length;
+}
+
+vi BFSShortestPath2(int s, int d) {
+    queue<int> q;
+    vi Length(adjlist.size(), INT_MAX);
+    vi Parents(adjlist.size(), -1);
+    Length[s] = 0;
+    bool found = false;
+    q.push(s);
+    int dep = 0, cur = s, size;
+    while (!q.empty() && !found) {
+        size = q.size();
+        while (size-- && !found) {
+            cur = q.front();
+            q.pop();
+            for (auto i: adjlist[cur]) {
+                if (i == d) {
+                    Parents[i] = cur;
+                    found = true;
+                    break;
+                }
+                if (Length[i] == INT_MAX) {
+                    q.push(i);
+                    Length[i] = dep + 1;
+                    Parents[i] = cur;
+                }
+            }
+        }
+        dep++;
+    }
+    vi Path;
+    while (d != -1) {
+        Path.push_back(Parents[d]);
+        d = Parents[d];
+    }
+    reverse(all(Path));
+    return Path;
+}
+
+bool kicked[101];
+int dx[8] = {1, 1, 1, -1, -1, -1, 0, 0};
+int dy[8] = {0, 1, -1, 0, 1, -1, 1, -1};
+int combinatorics[11][11];
+
+void solve() {
+    int x, y, z, w;
+    vi arr1, arr2, arr3;
+    cin >> x;
+    while (x--) {
+        cin >> w;
+        arr1.pb(w);
+    }
+    cin >> y;
+    while (y--) {
+        cin >> w;
+        arr2.pb(w);
+    }
+    cin >> z;
+    while (z--) {
+        cin >> w;
+        arr3.pb(w);
+    }
+    sort(all(arr1), greater<int>());
+    sort(all(arr2), greater<int>());
+    sort(all(arr3));
+    double r1=arr1[0],p1=arr2[0],p2=arr3[0];
+    double a,b;
+    cin >> a >> b;
+    double res = r1 * sqrt((b * p1) /( (a * p2) + (b * p1)));
+    cout.precision(12);
+    cout << fixed << res;
+}
+
+int main() {
+    IO
+    solve();
+}
